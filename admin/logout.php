918b5d9b006c78c2522720d6f0853407fefb32cc
Michal Reško <michal.resko.2@gmail.com>
2026-06-12 09:24:31 +0200

    logout.php

--- admin/logout.php
+++ admin/logout.php
@@ -1,15 +1,10 @@
 <?php
-// ============================================
-// admin/logout.php – Odhlásenie admina
-// ============================================
 require_once '../includes/bootstrap.php';
 
 $db   = Database::getConnection();
 $auth = new Auth($db);
 
-// Odhlás admina (zruší session)
 $auth->logout();
 
-// Presmeruj na login stránku
 setFlash('success', 'Bol si odhlásený.');
 redirect(BASE_URL . '/admin/login.php');
